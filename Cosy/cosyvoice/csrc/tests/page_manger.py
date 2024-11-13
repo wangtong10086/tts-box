86bf2421636450c4e0dd0d9a13db4c166003746f
bow rain <songruibao@xingzaitech.com>
2024-11-13 15:47:02 +0800

    coding for page manager

--- Cosy/cosyvoice/csrc/tests/page_manger.py
+++ Cosy/cosyvoice/csrc/tests/page_manger.py
@@ -6,7 +6,6 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-
 class FreePageQueue():
     def __init__(self, current_capacity):
         self.free_page_queue = deque(range(current_capacity))
@@ -16,12 +15,11 @@ class FreePageQueue():
             return self.free_page_queue.popleft()
         else:
             return -1
-    def enqueue_right(self, item):
-        self.free_page_queue.append(item)
+    def enqueue_right(self, phyiscal_page_id):
+        self.free_page_queue.append(phyiscal_page_id)
     
-    def batch_enqueue_right(self, items: list):
-        self.free_page_queue.extend(items)
-
+    def batch_enqueue_right(self, phyiscal_page_id_list: list):
+        self.free_page_queue.extend(phyiscal_page_id_list)
 
 
 class PageTableManager:
@@ -34,13 +32,16 @@ class PageTableManager:
         self.current_capacity = initial_pages
         self.dtype = dtype
 
-        # 页面池和页面跟踪
+        # 页面池
         self.page_pool = []
         for i in range(self.current_capacity):
             self.page_pool.append(torch.zeros((self.block_size, self.num_head, self.headsize), device='cuda', dtype=self.dtype))
+        # 物理页的引用计数，初始时为0
         self.page_usage = [0] * self.current_capacity
+        # 虚拟页--物理也映射表
         self.page_map = {}
-        self.remain_block = {}
+        # 未完全填充的逻辑页编号列表
+        self.remain_pages = []
         
         # 空闲 page 队列  -- 左侧出队，右侧入队
         self.free_page_queue = FreePageQueue(self.current_capacity)
@@ -106,9 +107,7 @@ class PageTableManager:
             
     
     def hash_block(self, block_token_idx: List[str]):
-        hash_id = ""
-        for token_idx in block_token_idx:
-            hash_id += token_idx
+        hash_id = '#'.join(block_token_idx)
         return hash_id
             
     def prefill(self, token_idx_list: list, data: torch.Tensor = None):
@@ -126,28 +125,80 @@ class PageTableManager:
         data = data.reshape(b*seq_len, num_head, head_size)
         
         # 划分 token_idx_list 为多个小块
-        blocks = [str(token_idx_list[i:i + self.block_size]) for i in range(0, len(token_idx_list), self.block_size)]
+        blocks = [token_idx_list[i:i + self.block_size] for i in range(0, len(token_idx_list), self.block_size)]
+
+        # 将每个子列表中的元素转换为字符串
+        blocks = [[str(item) for item in block] for block in blocks]
 
         # 检查最后一个块的大小，如果小于 block_size，则进行填充
         if len(blocks[-1]) < self.block_size:
             remain_len = self.block_size - len(blocks[-1])
-            blocks[-1] += ['M'] * remain_len  # 使用 += 避免多次调用 extend
+            blocks[-1].extend(['M'] * remain_len) 
 
         # 为每个块分配页面并填充
         for idx, block in blocks:
-            page_id = self.hash_block(block)
-            allocated_page_idx = self.allocate_page(page_id)
-            if allocated_page_idx == -1:
+            logical_page_id = self.hash_block(block)
+            if 'M' in logical_page_id:
+                self.remain_pages.append(logical_page_id)
+            phyiscal_page_idx = self.allocate_page(logical_page_id)
+            if phyiscal_page_idx == -1:
                 raise ValueError("Allocate memory out of max page size, Failed to allocate page for block:", block)
             
             # 对于每个块中的每个 token_idx，从 data 中提取对应的 token embedding
             start_pos = idx * self.block_size
             end_pos = min(start_pos + self.block_size, data.shape[0])
             data_block = data[start_pos:end_pos, :, :]
-            self.page_pool[allocated_page_idx] = data_block
+            self.page_pool[phyiscal_page_idx] = data_block
             
     
-    def decode(self, block_token_idx:list, data:torch.Tensor=None):
-        pass
+    def decode(self, block_token_idx:List[int], data:torch.Tensor=None):
+        assert data is not None, "data must be provided for decode"
+        current_token_id = block_token_idx[-1]
+        # 如果有未填满的page
+        if self.remain_pages:
+            # 找到未填满的page
+            logical_remain_page_id = self.remain_pages[0]
+            # 替换其中首个'M'字段，组成新的逻辑页id
+            new_page_id = logical_remain_page_id.replace('M', str(current_token_id), 1)
+            # 找到当前物理页id
+            phyiscal_current_page_id = self.page_map[logical_remain_page_id]
+            # 是否已经有对应的page -- 如果有, 直接复用
+            if new_page_id in self.page_map:
+                # 找到更新的物理页
+                phyiscal_update_page_id = self.page_map[new_page_id]
+                # 当前引用更新，不再引用旧的物理页
+                self.page_usage[phyiscal_current_page_id] -= 1
+                self.page_usage[phyiscal_update_page_id] += 1
+                # 检查先前的物理页是否有别的引用,如果没有
+                if self.page_usage[phyiscal_current_page_id] == 0:
+                   # 将先前的物理页放入空闲物理页队列中
+                   self.free_page_queue.enqueue_right(phyiscal_current_page_id)
+                   # 删除掉对应的逻辑页--物理页映射
+                   del self.page_map[logical_remain_page_id]
+            else: # 如果没有, 往未填满的page中插入
+                parts = logical_remain_page_id.split('#')
+                # 查找第一个 'M' 在列表中的位置
+                first_masked_pos = parts.index('M')
+                # 删除旧的逻辑页面id -- 物理页id映射，因为逻辑页面id要更换
+                del self.page_map[logical_remain_page_id]
+                # 更新逻辑页id的映射
+                self.page_map[new_page_id] = phyiscal_current_page_id
+                # 将数据插入page pool对应的位置当中
+                self.page_pool[phyiscal_current_page_id][first_masked_pos, :, :] = data 
+        else: # 如果没有，表示所有的物理页均已填满，要重新申请一个物理页
+            # 设置新申请物理页的逻辑页id
+            current_block_id = [str(current_token_id)]
+            if len(current_block_id) < self.block_size:
+                remain_len = self.block_size - len(current_block_id)
+                current_block_id.extend(['M'] * remain_len)
+            logical_page_id = self.hash_block(current_block_id)
 
-    
+            # 将对应的逻辑页追加到remain_pages中去
+            if 'M' in logical_page_id:
+                self.remain_pages.append(logical_page_id)
+            # 申请物理页
+            phyiscal_page_idx = self.allocate_page(logical_page_id)
+            if phyiscal_page_idx == -1:
+                raise ValueError("Allocate memory out of max page size, Failed to allocate page for block:", phyiscal_page_idx)
+            # 将数据插入page pool对应的位置当中
+            self.page_pool[phyiscal_current_page_id][first_masked_pos, :, :] = data 
